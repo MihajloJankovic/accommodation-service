036838431f38024cc13e9f24f6c7e94835cccc89
Pana77 <101635157+Pana77@users.noreply.github.com>
2023-11-05 20:12:40 +0100

    fix

--- protos/accommodation_service/app.pb.go
+++ protos/accommodation_service/app.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v4.25.0
 // source: app.proto
 
-package accommodation_service
+package protosgenerated
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
